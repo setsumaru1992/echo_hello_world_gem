454a529ff74746ea769dd1dd6bb3f9fe00e90ba8
setsumaru1992 <setsumaru@muriyari.work>
2020-03-14 15:05:12 +0900

    first commit

---
+++ lib/echo_hello_world_gem.rb
@@ -0,0 +1,6 @@
+require "echo_hello_world_gem/version"
+
+module EchoHelloWorldGem
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/echo_hello_world_gem/version.rb
@@ -0,0 +1,3 @@
+module EchoHelloWorldGem
+  VERSION = "0.1.0"
+end
